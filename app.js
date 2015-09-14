27ac6f5cdeaa6b32cd65707898505dc9db9d14f4
omeroot <demircanomer91@gmail.com>
2015-09-14 17:45:34 +0300

    leş

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var base64 = require('base64-stream');
 var fs = require('fs');
+
 var app = express();
 
 app.set('view engine', 'jade');
@@ -11,16 +13,14 @@ app.use(cookieParser());
 
 
 app.get('/image',function(req, res){
-  var stats = fs.statSync(__dirname + '/images/default1.jpg');
+  var stats = fs.statSync(__dirname + '/images/default.jpg');
   var fileSize = stats["size"];
 
   res.set('Content-Length',fileSize);
-  var counter = 0;
-  fs.createReadStream(__dirname + '/images/default1.jpg')
-      .on('data',function(data){
-        counter += 1;
-        console.log(counter);
-      })
+
+
+
+  fs.createReadStream(__dirname + '/images/default.jpg')
       .pipe(res)
       .on('finish',function(){
         res.end('thats it');
@@ -28,6 +28,27 @@ app.get('/image',function(req, res){
   //res.sendFile(__dirname + '/images/default.jpg');
 });
 
+app.get('/player/info',function(req, res){
+  res.json({
+    'duration' : '10:14'
+  });
+});
+
+app.get('/player',function(req, res){
+  var stats = fs.statSync(__dirname + '/images/music1.mp3');
+  var fileSize = stats["size"];
+
+  res.set('Content-Length',fileSize);
+
+
+
+  fs.createReadStream(__dirname + '/images/music1.mp3')
+      .pipe(res)
+      .on('finish',function(){
+        res.end('thats it');
+      });
+  //res.sendFile(__dirname + '/images/default.jpg');
+});
 
 
 app.listen(1337);
